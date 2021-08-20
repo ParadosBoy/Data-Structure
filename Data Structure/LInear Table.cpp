1b66793984f922485e225386e21a853f4fbd4d96
Honda <1057458756@qq.com>
2021-08-20 22:26:16 +0800

    完成题7

--- Data Structure/LInear Table.cpp
+++ Data Structure/LInear Table.cpp
@@ -205,3 +205,63 @@ bool Delete_same(SqList& L) {
 	return true;
 }//P17应用题6
 
+SqList Combine(SqList& L1, SqList& L2) {
+	SqList c;
+	int i = 0, j = 0, k = 0;
+	if (i < L1.length - 1 && j < L2.length - 1)
+	{
+		if (L1.data[i]<L2.data[j])
+		{
+			c.data[k++] = L1.data[i++];
+		}
+		else if (L1.data[i] < L2.data[i])
+		{
+			c.data[k++] = L2.data[j++];
+		}
+	}
+	if (i == L1.length - 1)
+	{
+		for (j; j < L2.length; j++)
+		{
+			c.data[k++] = L2.data[j];
+		}
+		return c;
+	}
+	else if (i == L2.length - 1)
+	{
+		for ( i; i < L1.length; i++)
+		{
+			c.data[k++] = L1.data[i];
+		}
+		return c;
+	}
+	else
+		return c;
+}//P17应用题7
+
+bool Exchange_or_Insert(SqList& L, ElemType x) {
+	int low = 0, mid = 0, high = 0;
+	high = L.length - 1;
+	if (L.data[L.length-1] == x) return;
+	while (low<high)
+	{
+		mid = (high + low) / 2;
+		if (L.data[mid] == x && mid != L.length - 1)
+		{
+			L.data[mid] = L.data[mid + 1];
+			L.data[mid + 1] = x;
+		}
+		else if (L.data[mid] < x)
+		{
+			low = mid + 1;
+		}
+		else
+		{
+			high = mid - 1;
+		}
+	}
+	for (int i = L.length - 1; i >= high; i--)
+		L.data[i + 1] = L.data[i];
+	L.data[high] = x;
+
+}//P17应用题9
