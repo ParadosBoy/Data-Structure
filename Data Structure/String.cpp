a7fc465b442959a0c8b13a8ad1299daa5da96646
Honda <1057458756@qq.com>
2021-08-22 19:22:31 +0800

    串的基本操作

--- Data Structure/String.cpp
+++ Data Structure/String.cpp
@@ -14,6 +14,84 @@ typedef struct {
 	int length;
 }HString;//堆分配存储
 
+int String_Sign(HString& S, char* ch) {
+	if (S.ch) 
+		free(S.ch);
+	int len = 0;
+	char* c = ch;
+	while (*c)
+	{
+		len++;
+		c++;
+	}
+	if (len==0)
+	{
+		S.ch = NULL;
+		S.length = 0;
+		return 1;
+	}
+	else
+	{
+		S.ch = (char*)malloc(sizeof(char) * (len + 1));
+		if (S.ch==NULL)
+		{
+			return 0;
+		}
+		else 
+		{
+			c = ch;
+			for (int i = 0; i <=len; i++,c++)
+			{
+				S.ch[i] = *c;
+			}
+			S.length = len;
+			return 1;
+		}
+	}
+}//赋值操作
+
+int Lenth(HString S) {
+	return S.length;
+}//取串长度
+
+int Compare(HString S1, HString S2) {
+	for (int i = 0; i < S1.length&&i<S2.length; i++)
+	{
+		if (S1.ch[i]!=S2.ch[i])
+		{
+			return S1.ch[i] - S2.ch[i];
+		}
+	}
+	return S1.length - S2.length;
+}//串比较
+
+int Concat(HString& S, HString S1, HString S2) {
+	if (S.ch)
+	{
+		free(S.ch);
+		S.ch = NULL;
+	}
+	S.ch = (char*)malloc(sizeof(char) * (S1.length+S2.length));
+	if (S.ch == NULL)
+	{
+		return 0;
+	}
+	int i = 0;
+	while (i<S1.length)
+	{
+		S.ch[i] = S1.ch[i];
+		i++;
+	}
+	int j = 0;
+	while (j <= S2.length)
+	{
+		S.ch[i+j] = S2.ch[j];
+		j++;
+	}
+	S.length = S1.length + S2.length;
+	return 1;
+}//串连接
+
 int Index(SString S, SString T) {
 	int i = 1, j = 1, k = 2;
 	while (i<=S.length&&j<=T.length)
@@ -34,3 +112,4 @@ int Index(SString S, SString T) {
 	else
 		return 0;
 }//简单模式匹配
+
