56eb9a1a62d13d7fc472157f7f11f9519f0a12f0
Honda <1057458756@qq.com>
2021-08-31 16:00:28 +0800

    折半插入排序

--- Data Structure/Sort.cpp
+++ Data Structure/Sort.cpp
@@ -5,13 +5,13 @@ using namespace std;
 typedef int ElemType;
 
 void InsertSort(ElemType A[], ElemType n) {
-	ElemType i, j;
-	for (i = 0; i <= n; i++)
+	ElemType i=0, j=1;
+	for (i = 2; i <= n; i++)
 	{
 		if (A[i] < A[i - 1])
 		{
 			A[0] = A[i];
-			for (j = j - 1; A[0] < A[j]; j--)
+			for (j = i - 1; A[0] < A[j]; j--)
 			{
 				A[j + 1] = A[j];
 			}
@@ -19,6 +19,33 @@ void InsertSort(ElemType A[], ElemType n) {
 		}
 	}
 }
+
+void InsertSort(ElemType A[], ElemType n) {
+	int i, j, low, high, mid;
+	for (i = 2; i <= n; i++)
+	{
+		A[0] = A[i];
+		low = 1;
+		high = i - 1;
+		while (low<high)
+		{
+			mid = (low + high) / 2;
+			if (A[mid]>A[0])
+			{
+				high = mid - 1;
+			}
+			else
+			{
+				low = mid + 1;
+			}
+			for (j = i - 1; j >= high; --j)
+			{
+				A[j + 1] = A[j];
+			}
+			A[high + 1] = A[0];
+		}
+	}
+}//’€∞Î≤Â»Î≈≈–Ú
 
 void QuickSort(ElemType A[], int low, int high) {
 	if (low<high)
