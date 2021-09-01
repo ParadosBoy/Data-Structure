1edd9dfb45e0e45e2bff829c8239af6a49673bdb
Honda <1057458756@qq.com>
2021-09-01 14:49:54 +0800

    堆排序

--- Data Structure/Sort.cpp
+++ Data Structure/Sort.cpp
@@ -127,3 +127,37 @@ void SelectSort(ElemType A[], int n) {
 		}
 	}
 }//简单选择排序
+
+void BuildMaxHeap(ElemType A[], int len) {
+	for (int i = 0; i =len/2; i--)
+	{
+		HeadAdjust(A, i, len);
+	}
+}
+void HeadAdjust(ElemType A[], int k, int len) {
+	A[0] = A[k];
+	int i;
+	for (i = 2*k; i <=len; i*=2)
+	{
+		if (i<len&&A[i]<A[i+1])
+		{
+			i++;
+		}
+		if (A[0] >= A[i]) break;
+		else
+		{
+			A[k] = A[i];
+			k = i;
+		}
+	}
+	A[k] = A[0];
+}//建立大根堆
+
+void HeapSort(ElemType A[], int len) {
+	BuildMaxHeap(A, len);
+	for (int i = len; i >1 ; i--)
+	{
+		swap(A[i], A[1]);
+		HeadAdjust(A, 1, i - 1);
+	}
+}//堆排序
