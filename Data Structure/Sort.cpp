f452c9602109e21071d71b63c966558f07dc84ea
Honda <1057458756@qq.com>
2021-09-01 14:18:22 +0800

    冒泡排序

--- Data Structure/Sort.cpp
+++ Data Structure/Sort.cpp
@@ -87,3 +87,24 @@ void ShellSort(ElemType A[], int n) {
 		}
 	}
 }//œ£∂˚≈≈–Ú
+
+void BubbleSort(ElemType A[], int n) {
+	int i, j;
+	bool flag;
+	for (i = 0; i < n - 1; i++)
+	{
+		flag = false;
+		for (j = n - 1; j > i; j--)
+		{
+			if (A[j-1]>A[j])
+			{
+				swap(A[j - 1], A[j]);
+				flag = true;
+			}
+		}
+		if (flag==false)
+		{
+			return;
+		}
+	}
+}//√∞≈›≈≈–Ú
